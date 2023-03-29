01a9a0c7571676b73bf65c50b6eb29f74ab64a4a
Firyuza91 <127919718+Firyuza91@users.noreply.github.com>
2023-03-29 22:21:33 +0300

    HW_3

---
+++ src/lesson_5/Array.java
@@ -0,0 +1,12 @@
+package lesson_5;
+
+public class Array {
+    public static void main(String[] args) {
+
+        byte a = 5;
+        byte [] array1 = new byte [7];
+        System.out.println("");
+
+
+    }
+}

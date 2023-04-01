aae0173164706dbf44227b288e75c3f2d369aeb0
Firyuza91 <127919718+Firyuza91@users.noreply.github.com>
2023-04-01 09:07:35 +0300

    HW_3

--- src/Lesson_4/hw_2.java
+++ src/Lesson_4/hw_2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class hw_2 {
     public static void main(String[] args) {
 
-        // Ввести с клавиатуры два числа m и n. Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+        // 1. C клавиатуры два числа m и n. Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
         // Пример: m=2, n=4
         // 8888
         // 8888
@@ -19,7 +19,7 @@ public class hw_2 {
         }
         System.out.println("Конец первой задачи");
 
-        //
+        // Вывести восьмерки
         //  8
         // 88
         // 888
@@ -44,37 +44,69 @@ public class hw_2 {
         // 3 Ввести с клавиатуры три числа, вывести на экран среднее из них. Т.е. не самое большое и не самое маленькое
         // Если все числа равны, вывести любое из них.
 
-        System.out.println("Введите три числа");
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[3];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        Arrays.sort(numbers);
-        System.out.println(numbers[1]);
-        scanner.close();
+        // System.out.println("Введите три числа");
+        // Scanner scanner = new Scanner(System.in);
+        // int[] numbers = new int[3];
+        //for (int i = 0; i < numbers.length; i++) {
+        //    numbers[i] = scanner.nextInt();
+        //}
+        // Arrays.sort(numbers);
+        // System.out.println(numbers[1]);
 
         System.out.println("Конец четвертой задачи");
+
+        //4
+        //Вывести на экран таблицу умножения 10х10 используя цикл while.
+        //Числа разделить пробелом.
+        //Пример вывода на экран:
+        //1 2 3 4 5 6 7 8 9 10
+        //2 4 6 8 10 12 14 16 18 20
+        //3 6 9 12 15 18 21 24 27 30
+        //4 8 12 16 20 24 28 32 36 40
+        //5 10 15 20 25 30 35 40 45 50
+        //6 12 18 24 30 36 42 48 54 60
+        //7 14 21 28 35 42 49 56 63 70
+        //8 16 24 32 40 48 56 64 72 80
+        //9 18 27 36 45 54 63 72 81 90
+        //10 20 30 40 50 60 70 80 90 100
+
+        int i = 1, n = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            while (n <= 9) {
+                n++;
+                System.out.print(i * n + " ");
+            }
+            i++;
+            n = 1;
+            System.out.println(" ");
+        }
+        System.out.println ("Конец 4 задачи");
+
+            // 5 Ввести с клавиатуры строку name. Ввести с клавиатуры дату рождения (три числа): y, m, d.
+            // Вывести на экран текст:
+            // "Меня зовут name.
+            // Я родился d.m.y"
+            // Пример вывода:
+            // Меня зовут Вася.
+            // Я родился 15.2.1988
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите имя");
+            String yorname = scanner.nextLine();
+
+            System.out.print("Введите дату рождения: ");
+            int date = scanner.nextInt();
+
+            System.out.print("Введите месяц рождения ");
+            int month = scanner.nextInt();
+
+            System.out.print("Введите год рождения ");
+            int year = scanner.nextInt();
+            System.out.print("Меня зовут" + " " + yorname + "." + "Я родился в" + " " + date + "." + month + "." + year);
+        }
     }
 
-        // 5 Ввести с клавиатуры строку name. Ввести с клавиатуры дату рождения (три числа): y, m, d.
-        // Вывести на экран текст:
-        // "Меня зовут name.
-        // Я родился d.m.y"
-        // Пример вывода:
-        // Меня зовут Вася.
-        // Я родился 15.2.1988
 
-         Scanner name = new Scanner(System.in);
-        System.out.println("Введите имя и дату рождения");
-        String yorname = scanner.nextLine();
-        System.out.print("Input age: ");
-        int date = scanner.nextInt();
-        System.out.print("Input date ");
-        System.out.printf("Мое имя" + yorname + " . " + "Я роидлся" +  date );
-        scanner.close();
-
-    }
-}
 
 
---
+++ src/lesson6/string.java
@@ -0,0 +1,9 @@
+package lesson6;
+
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        // отлавливать текст с экрана и перевернуть
+    }
+}
